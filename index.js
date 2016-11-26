e53f8a1149056ebecc26c27396ecef7fe54d2c59
ChristianJeday <cesp_193@hotmail.com>
2016-11-26 10:08:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express=require('express');
 var app=express();
 var http=require('http').Server(app);
-
+var io = require('socket.io')(http);
 //usando socket io
 var io=require('socket.io')(http);
 
@@ -17,4 +17,20 @@ app.get('/',function(req,res){
 http.listen(port, function(){//redirigiendo
 	console.log('Escuchando en el puerto: ' +port);
 
+});
+
+var socketCount = 0; //Contador de conexiones al server
+io.on('connection', function()){
+      console.log('Usuario conectado...');
+	socketCount++;
+
+//emitiendo mensaje a todos los socketsno usuarios conectados
+	io.sockets.emit('usuario conectado', socketCount);
+      
+      });//cierra conexion
+
+socket.on('disconnect', function(){
+	socketCount--;//decremento del contador
+	console.log('Usuario Desconectado');//mensaje en consola
+	io.socket.emit('usuario desconectado');//mensaje a todos los sockets
 });
